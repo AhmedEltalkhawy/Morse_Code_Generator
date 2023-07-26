75acdbe781ff8f1ffa20027b9849765a546d1058
ahmed eltalkhawy <ahmedeltalkhawy962@gmail.com>
2023-07-26 21:02:06 +0300

    ITI project

---
+++ Code/002-SLAVE/Debug/Timers_Services.d
@@ -0,0 +1,17 @@
+Timers_Services.o Timers_Services.o: ../Timers_Services.c \
+  ../Timers_Services.h ../DIO_Interface.h ../MemMAP.h ../Utils.h \
+  ../StdTypes.h ../Timers.h ../LCD.h
+
+../Timers_Services.h:
+
+../DIO_Interface.h:
+
+../MemMAP.h:
+
+../Utils.h:
+
+../StdTypes.h:
+
+../Timers.h:
+
+../LCD.h:
